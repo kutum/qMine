5b12fc7979948183a2e45a70e61df99fcdad08d1
drago <dragono0146@gmail.com>
2019-09-03 19:51:20 +0400

    make blank website add rcon lib add rcon test console

---
+++ qMIne/qMIne/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="qMIne.MvcApplication" Language="C#" %>
---
+++ qMIne/rconConsole/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MinecraftServerRCON; 
+
+namespace rconConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var rcon = RCONClient.INSTANCE)
+            {
+                rcon.setupStream("192.168.1.15", password: "myofrene");
+                var answer = rcon.sendMessage(RCONMessageType.Command, "help 1");
+                Console.WriteLine(answer.RemoveColorCodes());
+                Console.ReadKey();
+            }
+        }
+    }
+}
